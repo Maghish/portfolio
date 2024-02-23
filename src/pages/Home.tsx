92148fa00b93b0524d3bc842683a11a7d4e1ede1
Maghish <mail4mithran@gmail.com>
2024-02-23 14:23:05 +0530

    Updated frontend to adopt TS pt-2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
-import GetIconValue from "../hooks/GetIconValue";
-
 import Navbar from "../components/Navbar";
 
 function Home() {
   const [hovering, setHovering] = useState<null | boolean>(null);
-
+  console.log(hovering);
   const manageHover = (e: any) => {
     setHovering(e.target.id);
   };
