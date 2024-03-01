127d496e4454a8fdbdd9159d9ade771ffd4b8c3e
Maghish <mail4mithran@gmail.com>
2024-03-01 14:50:30 +0530

    Implemented Statsbar

--- src/components/Statsbar.tsx
+++ src/components/Statsbar.tsx
@@ -1,16 +1,18 @@
 function Statsbar() {
   return (
-    <div className="bg-bgThemeGray border-y-2 border-r-2 border-borderDarkerGreenColor max-w-[400px] h-[560px] max-h-[560px] p-5">
-      <p className="text-themeLightGray font-consolas font-semibold">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi nobis id
-        rem officiis maxime necessitatibus quasi neque. Quae distinctio fugiat,
-        repudiandae ad, sed quam nulla voluptate officia commodi, doloribus
-        atque earum ducimus minus sapiente. Explicabo dicta perferendis debitis
-        autem reiciendis, repellat, nisi ex vitae maiores labore eligendi
-        doloremque delectus vel illo, voluptatum optio inventore asperiores
-        quae! Tempore molestias suscipit deserunt blanditiis voluptates nesciunt
-        dolore voluptatum, cupiditate alias, libero quo.
-      </p>
+    <div className="bg-bgThemeGray border-y-2 border-r-2 border-borderDarkerGreenColor max-w-[400px] h-[560px] max-h-[560px] p-5 flex flex-col justify-around">
+      <img
+        src="https://github-readme-activity-graph.vercel.app/graph?username=Maghish&height=600&bg_color=353638&color=cbcdd2&line=a1ffef7e&point=a1ffefb9&border_color=a1ffef7e&area=true&hide_border=false"
+        alt="activityGraph"
+      ></img>
+      <img
+        src="https://github-readme-stats.vercel.app/api?username=Maghish&show_icons=true&title_color=DCDFE4&text_color=cbcdd2&icon_color=a1ffefb9&border_color=a1ffef7e&bg_color=353638"
+        alt="statsCard"
+      ></img>
+      <img
+        src="https://streak-stats.demolab.com?user=Maghish&border_radius=2.6&border=A1FFEF7E&background=3536387E&stroke=A1FFEF7E&ring=A1FFEFB9&currStreakNum=CBCDD2&sideNums=CBCDD2&sideLabels=CBCDD2&fire=A1FFEFB9&currStreakLabel=CBCDD2&dates=CBCDD2&excludeDaysLabel=CBCDD2&hide_longest_streak=true"
+        alt="streakCard"
+      ></img>
     </div>
   );
 }
