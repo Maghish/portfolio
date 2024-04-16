b417b76c9a8dc3f3566e23e0499da8d875150c49
Maghish <mail4mithran@gmail.com>
2024-04-16 13:59:05 +0530

    Fixed techstaacktag oversize

--- src/components/TechStackTag.tsx
+++ src/components/TechStackTag.tsx
@@ -2,7 +2,7 @@ import { TechStackTagComponent } from "../vite-env";
 
 function TechStackTag({ name }: TechStackTagComponent) {
   return (
-    <div className="bg-borderContrastGreenColor font-extrabold font-consolas text-black h-fit w-fit px-2 rounded-md cursor-pointer mr-2 mb-2 transition delay-75 ease-out hover:scale-[1.1]">
+    <div className="bg-borderContrastGreenColor font-extrabold font-consolas text-black text-sm sm:text-base h-fit w-fit px-2 rounded-md cursor-pointer mr-2 mb-2 transition delay-75 ease-out hover:scale-[1.1]">
       {name}
     </div>
   );
