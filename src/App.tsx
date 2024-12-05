2225e1dcbcdc96ff23b8eabfef096a80b533d389
Maghish <mail4mithran@gmail.com>
2024-12-05 12:51:23 +0530

    Reset once again for new theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./css/index.css";
 
-import Body from "./components/Body";
-
 function App() {
-  return (
-    <div className="min-h-screen h-screen w-full pt-12 p-6 md:p-auto md:pt-24 md:pb-12 lg:p-24">
-      <Body />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
