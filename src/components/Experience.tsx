0ad9b17a263a9f7222ece0c414dac39ce5f9681a
Maghish <mail4mithran@gmail.com>
2024-12-20 23:12:25 +0530

    feat: created experience component

---
+++ src/components/Experience.tsx
@@ -0,0 +1,8 @@
+export default function Experience() {
+  return (
+    <div className="relative hidden md:flex w-full min-h-[500px] max-h-[600px] justify-center pt-16 items-end">
+      <div className="z-10 rounded-full w-4 h-4 bg-[#8B5CF6]" />
+      <div className="absolute left-1/2 -translate-x-1/2 w-[4px] bg-[#686868] transition-all duration-500 ease-in-out h-[30px] hover:!h-full" />
+    </div>
+  );
+}
