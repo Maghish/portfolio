f927a049f5552abd47fdd0f805aa72d3a6ea3b49
Maghish <mail4mithran@gmail.com>
2024-12-27 12:35:59 +0530

    feat: added tooltip to every experience button on horizontal experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,24 +3,10 @@ import HorizontalExp from "./subcomponents/HorizontalExp";
 import VerticalExp from "./subcomponents/VerticalExp";
 
 export default function Experience() {
-  // const [isLgScreen, setIsLgScreen] = useState(window.innerWidth <= 1024);
-
-  // const handleResize = () => {
-  //   setIsLgScreen(window.innerWidth >= 1024);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize);
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
-
   return (
     <>
       <HorizontalExp />
       <VerticalExp />
     </>
   );
-  // return <></>;
 }
--- src/components/subcomponents/HorizontalExp.tsx
+++ src/components/subcomponents/HorizontalExp.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
 
 export default function HorizontalExp() {
   const experienceRef = useRef<HTMLDivElement>(null);
@@ -34,44 +35,23 @@ export default function HorizontalExp() {
       className="relative flex lg:hidden flex-row w-full min-h-[200px] max-h-[200px] pt-5 items-center justify-start"
     >
       <div className="flex flex-row w-full min-h-100% items-center justify-evenly">
-        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B5CF6] cursor-pointer transition-all duration-[150ms] hover:scale-150">
-          {/* <div
-            className={`relative bottom-0 z-0 rounded-md bg-[#686868] transition-all delay-[1100ms] duration-[600ms] ease-linear w-[4px] h-0 ${
-              viewing ? "!h-[16px] translate-y-[16px]" : ""
-            }`}
-          >
-            <div
-              className={`relative top-[15px] -left-[100px] z-10 rounded-md bg-[#ff0404] transition-all delay-[1600ms] duration-[200ms] ease-linear h-[100px] w-[200px] ${
-                viewing ? "!opacity-100" : "opacity-0"
-              }`}
-            ></div>
-          </div> */}
+        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B5CF6] cursor-pointer transition-all duration-[150ms] hover:scale-150 group">
+          <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 mb-5 w-max p-2 bg-gray-700 text-white text-xs rounded opacity-0 group-hover:opacity-100 group-hover:!scale-75">
+            Click to view!
+            <FaCaretDown className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 text-gray-700" />
+          </div>
         </div>
-        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B5CF6]  cursor-pointer transition-all duration-[150ms] hover:scale-150">
-          {/* <div
-            className={`relative top-0 z-0 rounded-md bg-[#686868] transition-all delay-[1100ms] duration-[600ms] ease-linear w-[4px] h-0 ${
-              viewing ? "!h-[16px] -translate-y-[16px]" : ""
-            }`}
-          >
-            <div
-              className={`relative right-[-50px] top-[-50px] z-10 rounded-md bg-[#686868] transition-all delay-[1600ms] duration-[200ms] ease-linear h-[100px] w-[200px] ${
-                viewing ? "!opacity-0" : "opacity-0"
-              }`}
-            ></div>
-          </div> */}
+        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B5CF6] cursor-pointer transition-all duration-[150ms] hover:scale-150 group">
+          <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 mb-5 w-max p-2 bg-gray-700 text-white text-xs rounded opacity-0 group-hover:opacity-100 group-hover:!scale-75">
+            Click to view!
+            <FaCaretDown className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 text-gray-700" />
+          </div>
         </div>
-        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B4CF6]  cursor-pointer transition-all duration-[150ms] hover:scale-150">
-          {/* <div
-            className={`relative bottom-0 z-0 rounded-md bg-[#686868] transition-all delay-[1100ms] duration-[600ms] ease-linear w-[4px] h-0 ${
-              viewing ? "!h-[16px] translate-y-[16px]" : ""
-            }`}
-          >
-            <div
-              className={`relative top-[15px] -left-[100px] z-10 rounded-md bg-[#ff0000] transition-all delay-[1600ms] duration-[200ms] ease-linear h-[100px] w-[200px] ${
-                viewing ? "!opacity-100" : "opacity-0"
-              }`}
-            ></div>
-          </div> */}
+        <div className="relative z-10 rounded-full w-4 h-4 bg-[#8B4CF6] cursor-pointer transition-all duration-[150ms] hover:scale-150 group">
+          <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 mb-5 w-max p-2 bg-gray-700 text-white text-xs rounded opacity-0 group-hover:opacity-100 group-hover:!scale-75">
+            Click to view!
+            <FaCaretDown className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 text-gray-700" />
+          </div>
         </div>
       </div>
 
