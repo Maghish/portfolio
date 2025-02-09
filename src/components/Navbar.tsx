bf6bcb51fb6c07d0105721ef7740ffdb9e96038b
Maghish <mail4mithran@gmail.com>
2025-02-09 16:10:36 +0530

    refactor: enhance Navbar component with improved hover effects and layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 z-50 bg-[#FFFFFF] bg-opacity-10 backdrop-blur-xl w-full h-[50px] flex flex-row items-center justify-center space-x-10">
-      <span className="cursor-pointer bg-clip-text text-transparent font-inter-bold bg-gradient-to-r from-white via-white to-white transition duration-300 ease-in-out hover:from-purple-500 hover:via-violet-500 hover:to-indigo-500">
-        About Me
-      </span>
-      <span className="cursor-pointer bg-clip-text text-transparent font-inter-bold bg-gradient-to-r from-white via-white to-white transition duration-300 ease-in-out hover:from-purple-500 hover:via-violet-500 hover:to-indigo-500">
-        Projects
-      </span>
-      <span className="cursor-pointer bg-clip-text text-transparent font-inter-bold bg-gradient-to-r from-white via-white to-white transition duration-300 ease-in-out hover:from-purple-500 hover:via-violet-500 hover:to-indigo-500">
-        Contact
-      </span>
+      <div className="relative inline-block group">
+        <span className="relative z-10 cursor-pointer text-white font-inter-bold transition duration-300 ease-in-out group-hover:text-black">
+          About Me
+        </span>
+        <span className="absolute inset-y-0 left-[-4px] right-[-4px] bg-gradient-to-r from-purple-500 via-violet-500 to-indigo-500 bg-opacity-75 transform origin-left scale-x-0 transition-transform duration-300 pointer-events-none group-hover:scale-x-[1] rounded"></span>
+      </div>
+      <div className="relative inline-block group">
+        <span className="relative z-10 cursor-pointer text-white font-inter-bold transition duration-300 ease-in-out group-hover:text-black">
+          Projects
+        </span>
+        <span className="absolute inset-y-0 left-[-4px] right-[-4px] bg-gradient-to-r from-purple-500 via-violet-500 to-indigo-500 bg-opacity-75 transform origin-left scale-x-0 transition-transform duration-300 pointer-events-none group-hover:scale-x-[1] rounded"></span>
+      </div>
+      <div className="relative inline-block group">
+        <span className="relative z-10 cursor-pointer text-white font-inter-bold transition duration-300 ease-in-out group-hover:text-black">
+          Contact
+        </span>
+        <span className="absolute inset-y-0 left-[-4px] right-[-4px] bg-gradient-to-r from-purple-500 via-violet-500 to-indigo-500 bg-opacity-75 transform origin-left scale-x-0 transition-transform duration-300 pointer-events-none group-hover:scale-x-[1] rounded"></span>
+      </div>
     </div>
   );
 }
