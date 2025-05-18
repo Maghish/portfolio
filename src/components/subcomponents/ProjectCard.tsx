fe58a4f4d2e952d68215ff2eb9473064fddda7f8
Maghish <mail4mithran@gmail.com>
2025-05-18 13:56:24 +0530

    feat: modified project tech stack tag gap

--- src/components/subcomponents/ProjectCard.tsx
+++ src/components/subcomponents/ProjectCard.tsx
@@ -12,14 +12,14 @@ export default function ProjectCard({
   projectTags: string[];
 }) {
   return (
-    <div className="bg-[#191919] px-[25px] md:px-[40px] py-[35px] rounded-xl cursor-pointer flex flex-col gap-y-[20px] flex-shrink-0 flex-grow-0 flex-basis-[290px] min-w-[290px] max-w-[290px] md:flex-basis-[320px] md:w-[320px] md:min-w-[320px] md:max-w-[320px] min-h-[300px] max-h-[300px] md:min-h-[330px] md:max-h-[330px] transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-tech-stack-tag-shadow">
+    <div className="bg-[#121010] px-[25px] md:px-[40px] py-[35px] rounded-xl cursor-pointer flex flex-col gap-y-[20px] flex-shrink-0 flex-grow-0 flex-basis-[290px] min-w-[290px] max-w-[290px] md:flex-basis-[320px] md:w-[320px] md:min-w-[320px] md:max-w-[320px] min-h-[300px] max-h-[300px] md:min-h-[330px] md:max-h-[330px] transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-tech-stack-tag-shadow">
       <h4 className="font-inter-bold text-white text-lg text-center md:text-start">
         {projectName}
       </h4>
       <p className="font-inter-regular text-white text-sm min-h-[95px] max-h-[95px] md:min-h-[125px] md:max-h-[125px] overflow-y-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
         {projectDescription}
       </p>
-      <div className="w-full flex flex-wrap gap-x-[5px] gap-y-[5px]">
+      <div className="w-full flex flex-wrap gap-x-[10px] md:gap-x-[5px] gap-y-[5px]">
         {projectTags.map((tag) => (
           <ProjectStackTag tag={tag} />
         ))}
